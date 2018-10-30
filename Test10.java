588ce3d22232fe6090ef17b77d35353f92260686
佐々木喜光 <goodriddance.ys@gmail.com>
2018-10-30 23:54:13 +0900

    メソッド基礎 その３ インスタンスメソッド・コンストラクタ

--- Test10.java
+++ Test10.java
@@ -42,20 +42,20 @@ ss[0] = new Student("Taro", 30);
 
 public class Test10{
   public static void main(String[] args) {
-    Student[] ss = new Student[3];
+    Student[] ss = new Student[3];          // 配列を作成
     //ss[0] = new Student(); ss[0].name = "Taro"; ss[0].age = 19;
-    ss[0] = new Student("Taro", 30);
+    ss[0] = new Student("Taro", 30);        // 配列の要素を作成し、代入
     //ss[1] = new Student(); ss[1].name = "Hanako"; ss[1].age = 20;
     ss[1] = new Student("Hanako", 22);
     //ss[2] = new Student(); ss[2].name = "Jiro"; ss[2].age = 18;
     ss[2] = new Student("Jiro", 18);
 
-    ss[0].printInfo();
+    ss[0].printInfo();                      // インスタンスメソッドの呼び出し
     ss[1].printInfo();
     ss[2].printInfo();
 
 
-    Rectangle r = new Rectangle(0.5, 3.13);
+    Rectangle r = new Rectangle(0.5, 3.13); // インスタンス化と代入を同時に行う
     System.out.println("rの幅は " + r.width + " rの高さは " +
                         r.height + " です。");
   }
@@ -70,7 +70,7 @@ class Student {
     this.age = age;
   }
 
-  void printInfo(){
+  void printInfo(){                 // thisを使って名前と歳を出力
     System.out.println("私の名前は " + this.name + " です。" +
                       this.age + "歳です。");
   }
@@ -80,10 +80,8 @@ class Rectangle {
   double width;
   double height;
 
-  Rectangle(double width, double height){
+  Rectangle(double width, double height){ // コンストラクタ
     this.width = width;
     this.height = height;
   }
-
-
 }
